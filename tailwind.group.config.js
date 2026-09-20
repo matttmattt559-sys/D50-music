6d0dde4a85c8faafe44c9c960e8a970f399f1a03
matttmattt559-sys <matttmattt559@gmail.com>
2026-09-20 03:51:09 +0200

    Add files via upload

---
+++ tailwind.group.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./public/script.js"],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+};
